5fede2c0a5bd1305354d610a6d4a5793a8ad5763
LeoHuang0511 <leo95033@gmail.com>
2024-01-19 18:58:09 +0800

    fix above bug

--- model/dcn.py
+++ model/dcn.py
@@ -130,7 +130,9 @@ class MultiColumnOffsetConv(nn.Module):
 
     def forward(self, x):
         # x1 = self.conv1(x)
-        x2 = torch.cat([self.column1(x1), self.column2(x1), self.column3(x1)], dim=1)
+        # x2 = torch.cat([self.column1(x1), self.column2(x1), self.column3(x1)], dim=1)
+        x2 = torch.cat([self.column1(x), self.column2(x), self.column3(x)], dim=1)
+
         # x2 = self.conv2(x2)
         # x3 = self.offset_conv(x1 + x2)
         x3 = self.offset_conv(x2)
