a5a38d0a78e8cdfca71934b9c64ee3526bd9be5c
Leo Huang <leo95033@gmail.com>
2024-02-06 06:54:52 +0000

    #

--- train.py
+++ train.py
@@ -696,7 +696,7 @@ if __name__=='__main__':
 
     #_test or val
     parser.add_argument('--VAL_FREQ', type=int, default=1000)
-    parser.add_argument('--VAL_START', type=int, default=1)
+    parser.add_argument('--VAL_START', type=int, default=0)
     parser.add_argument('--VAL_BATCH_SIZE', type=int, default=1)
     parser.add_argument('--VAL_INTERVALS', type=int, default=50)
 
