788873840a6a6da13d22464efed33d2a0859875c
Leonel Huang <114064726+LeoHuang0511@users.noreply.github.com>
2025-03-04 17:42:33 +0800

    Update samplers.py

--- datasets/samplers.py
+++ datasets/samplers.py
@@ -1,14 +1,4 @@
-# +
-# #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-## Created by: Yaoyao Liu
-## Modified from: https://github.com/Sha-Lab/FEAT
-## Tianjin University
-## liuyaoyao@tju.edu.cn
-## Copyright (c) 2019
-##
-## This source code is licensed under the MIT-style license found in the
-## LICENSE file in the root directory of this source tree
-# #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
 """ Sampler for dataloader. """
 import torch
 import numpy as np
