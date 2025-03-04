85cf6dce9804af0aa7829cdf501e942db412511f
Leonel Huang <114064726+LeoHuang0511@users.noreply.github.com>
2025-03-04 19:54:28 +0800

    Update prroi_pooling_gpu.h

--- model/PreciseRoIPooling/pytorch/prroi_pool/src/prroi_pooling_gpu.h
+++ model/PreciseRoIPooling/pytorch/prroi_pool/src/prroi_pooling_gpu.h
@@ -1,12 +1,4 @@
-/*
- * File   : prroi_pooling_gpu.h
- * Author : Jiayuan Mao, Tete Xiao
- * Email  : maojiayuan@gmail.com, jasonhsiao97@gmail.com 
- * Date   : 07/13/2018
- * 
- * Distributed under terms of the MIT license.
- * Copyright (c) 2017 Megvii Technology Limited.
- */
+
 
 int prroi_pooling_forward_cuda(THCudaTensor *features, THCudaTensor *rois, THCudaTensor *output, int pooled_height, int pooled_width, float spatial_scale);
 
