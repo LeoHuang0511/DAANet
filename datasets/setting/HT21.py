90574df19daa620b4494dfafe901750f144079e6
Leonel Huang <114064726+LeoHuang0511@users.noreply.github.com>
2025-03-04 17:40:04 +0800

    Update HT21.py

--- datasets/setting/HT21.py
+++ datasets/setting/HT21.py
@@ -7,7 +7,6 @@ __C_HT21 = edict()
 cfg_data = __C_HT21
 
 __C_HT21.TRAIN_SIZE =(768,1024) # (848,1536) #
-# __C_HT21.DATA_PATH = '/nfs/home/leo0511/Research/datasets/HT21'
 __C_HT21.DATA_PATH = '../../datasets/HT21/'
 __C_HT21.TRAIN_LST = 'train.txt'
 __C_HT21.VAL_LST =  'val.txt'
